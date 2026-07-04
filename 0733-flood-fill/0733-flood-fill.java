997f7da250e6c86ff9863ef5b21918aeebe2de5e
Ishan Katlam <ishanknjr@gmail.com>
2026-07-04 15:01:04 +0530

    Time: 0 ms (100%), Space: 46.4 MB (95.23%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int ogcolor = image[sr][sc];
-        if(color == ogcolor)
+        if(color == ogcolor )
         {
-            return image;
+            return image ;
         }
-       dfs(image , sr , sc , ogcolor , color);
-       return image ;
+        dfs(image , sr , sc, ogcolor , color);
+        return image ;
     }
-
-    public void dfs(int[][] image ,int r , int c , int ogcolor , int newcolor)
+    public void dfs(int[][] image , int r, int c, int ogcolor , int newcolor )
     {
-        if(r<0 || c < 0 || r>=image.length || c>=image[0].length)
+        if(r<0 || c<0 || r>=image.length|| c>=image[0].length)
         {
             return ;
         }
-        if(image[r][c] != ogcolor)
+
+        if(image[r][c]!=ogcolor)
         {
-            return ;
+          return ;
+
         }
 
         image[r][c] = newcolor;
 
-        dfs(image ,r-1 , c , ogcolor , newcolor );//up
-        dfs(image , r+1 , c , ogcolor , newcolor);// down
-        dfs(image , r , c-1 , ogcolor , newcolor); // left
-        dfs(image , r ,c+1 , ogcolor , newcolor); // right
+        dfs(image , r-1, c, ogcolor , newcolor);
+         dfs(image , r+1, c, ogcolor , newcolor);
+          dfs(image , r, c-1, ogcolor , newcolor);
+           dfs(image , r, c+1, ogcolor , newcolor);
     }
 }
