54ba63546e1317b3b1fb7124b70c0d4889cd9093
Ishan Katlam <ishanknjr@gmail.com>
2026-09-04 12:59:11 +0530

    Time: 11 ms (38.2%), Space: 70.7 MB (22.1%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,78 +1,34 @@
 class Solution {
-     class DSU{
-    int[] parent , size;
-
-    DSU(int n){
-       parent = new int[n];
-       size = new int[n];
-
-       for(int i=0;i<n;i++)
-       {
-        parent[i] = i;
-        size[i] = 1;
-       }
-    }
-
-
-    int findparent(int node)
-    {
-        if(parent[node] == node)return node;
-        return parent[node] = findparent(parent[node]);
-    }
-
-
-
-    void unionBySize(int u , int v)
-    {
-        int pu = findparent(u);
-        int pv = findparent(v);
-
-        if(pu==pv)return ;
-
-        if(size[pu] < size[pv]){
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        }
-        else 
-        {
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-    }
-
-
-    }
     public int makeConnected(int n, int[][] connections) {
-         if(connections.length < n-1)
-         {
-            return -1;
-         }
+        if(connections.length < n-1) return -1;
 
-         DSU ds = new DSU(n);
+        List<Integer>[] graph = new List[n];
 
-         int extra = 0;
+        for(int i=0;i<n;i++)graph[i] = new ArrayList<>();
 
-         for(int[] e : connections)
-         {
-            int u = e[0];
-            int v = e[1];
+        for(int[] c : connections)
+        {
+            graph[c[0]].add(c[1]);
+            graph[c[1]].add(c[0]);
+        }
 
-            if(ds.findparent(u) == ds.findparent(v))
-            {
-                extra++;
-            }
-            else
+        int components = 0;
 
-            ds.unionBySize(u , v);
-         }
+        boolean[] visited = new boolean[n];
+        for(int i=0;i<n;i++)
+        {
+            components += dfs(i , graph , visited);
+        }
+        return components -1 ;
+    }
 
-         int components =0;
+    public int dfs(int u , List<Integer>[] graph , boolean[] visited)
+    {
+        if(visited[u]) return 0;
 
-         for(int i=0;i<n;i++)
-         {
-            if(ds.findparent(i) == i)components++;
-         }
+        visited[u] = true ;
 
-         return extra >= components -1 ? components-1: -1;
+        for(int v : graph[u]) dfs(v , graph , visited);
+        return 1 ;
     }
 }
